8e2b80a381b6ee049451192e9aebef9f5fc1d954
abolfazlGit <126224475+abolfazlGit@users.noreply.github.com>
2023-02-23 21:05:35 -0800

    Add files via upload

---
+++ admin/master.blade.php
@@ -0,0 +1,17 @@
+//create
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style">
+    @stack('name')
+</head>
+<body>
+    <div class="comtainer">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ index.blade.php
@@ -0,0 +1,11 @@
+//use
+@extends('master')
+
+////////////////////////////
+@push('name')
+<link rel="stylesheet" href="bott">
+@endpush
+////////////////
+
+@section('content')
+@endsection
